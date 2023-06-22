f5684d12d84867236f7ee2a8aa22442054e707e5
ahusu <su.aust@gmail.com>
2023-06-22 16:30:13 -0700

    created some boilerplate code for react components rendered to the dom for the Outfits list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import OutfitsList from './relatedProducts/OutfitsList.jsx'
 
 export default function App() {
 
 return (
   <div>
     <h1>Hello World</h1>
+
+    <OutfitsList />
   </div>
 )
 };
