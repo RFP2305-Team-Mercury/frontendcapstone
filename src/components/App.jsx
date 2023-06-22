d5bc0f5b1d3b7d10622b84203d349e5fa5f3ccaa
carolinerobbins <caroline.tawney@gmail.com>
2023-06-22 16:33:53 -0700

    updated with componenets with basic html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Details from './overview/Details.jsx'
 
 
 export default function App() {
@@ -6,6 +7,8 @@ export default function App() {
 return (
   <div>
     <h1>Hello World</h1>
+    <Details />
+    <Gallery />
   </div>
 )
 };
