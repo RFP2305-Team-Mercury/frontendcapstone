00e78f7740f4ad0dd281e952a19f7815a918f948
pdaly91 <patrickdaly91@gmail.com>
2023-06-25 16:51:35 -0700

    add formatting for review tiles

--- src/components/ratingsAndReviews/ReviewTile.jsx
+++ src/components/ratingsAndReviews/ReviewTile.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { format, parseISO } from 'date-fns';
 
 const ReviewTile = ({review}) => {
+  let formattedDate = format(parseISO(review.date), 'MMMM dd, yyyy');
+
   return (
-    <div>
-      <h3>Review Tile</h3>
-      <p>Summary: {review.summary}</p>
-      <p>Body: {review.body}</p>
-      <p>By: {review.reviewer_name}</p>
+    <div className="border-b-2 border-gray-800 py-4 mb-4">
+      <div className="flex justify-between">
+        <span>Stars: {review.rating}</span>
+        <span className="font-thin">{review.reviewer_name}, {formattedDate}</span>
+      </div>
+      <div className="py-2">
+        <p className="text-lg text-gray-800 font-bold">{review.summary}</p>
+        <p>{review.body}</p>
+      </div>
+      {review.recommend ? <div>&#10003; I recommend this product</div> : ""}
+      <div className="font-light text-gray-600 py-2">
+        <span className="mr-1">Helpful?
+          <span className="underline mx-2">Yes</span>
+          <span>({review.helpfulness})</span>
+        </span>
+          <span className="mx-4 text-xl font-thin">|</span>
+          <span className="underline">Report</span>
+      </div>
     </div>
   );
 };
