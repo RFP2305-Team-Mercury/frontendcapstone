fe2e311ff7825ae7e1fa36d07342e6f9a83db176
pdaly91 <patrickdaly91@gmail.com>
2023-06-26 14:58:09 -0700

    added star rating view

--- src/components/ratingsAndReviews/RatingSummary.jsx
+++ src/components/ratingsAndReviews/RatingSummary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 const { useState, useEffect } = React;
 
@@ -16,6 +17,7 @@ const RatingSummary = ({metaData}) => {
       total += (Number(newCount) * i);
     }
     let calcAvg = (total / calcCount).toPrecision(2);
+    calcAvg = (Math.round(calcAvg * 4) / 4).toPrecision(3)
     let calcRecPct = (Number(metaData.recommended.true) / (Number(metaData.recommended.true) + Number(metaData.recommended.false))).toPrecision(3);
 
     let result = {calcCount, calcAvg, calcRecPct};
@@ -41,8 +43,16 @@ const RatingSummary = ({metaData}) => {
 
   return (
     <div className="w-4/12 flex flex-col">
-      <div className="text-5xl font-bold my-4">
+      <div className="text-5xl font-bold my-4 flex flex-row place-items-start">
         {average}
+        <div className="relative -top-[16px] left-2">
+          {average ? <StarRatings
+            rating={Number(average)}
+            numberOfStars={5}
+            starDimension="15px"
+            starSpacing="1px"
+          /> : ''}
+        </div>
       </div>
       <div className="my-2">
         {recPct * 100}% of reviews recommend this product
