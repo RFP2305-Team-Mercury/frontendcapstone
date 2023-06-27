e923d1e6fdb9150f731f20d1d299ee5cbe61ede4
carolinerobbins <caroline.tawney@gmail.com>
2023-06-27 11:09:17 -0700

    added thumbnail overlay and click functionality to gallery

--- src/components/overview/Gallery.jsx
+++ src/components/overview/Gallery.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Gallery() {
-//receive style from props
-//handleClick = if thumbnail is clicked, use conditional rendering to change big img to selected photo
+const photoArray = [
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1554260570-9140fd3b7614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1554260570-9140fd3b7614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
+  },
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1553830591-2f39e38a013c?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1553830591-2f39e38a013c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2760&q=80"
+  },
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1511&q=80"
+  },
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+      "thumbnail_url": "https://images.unsplash.com/photo-1554774853-d50f9c681ae2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+      "url": "https://images.unsplash.com/photo-1554774853-d50f9c681ae2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80"
+  }
+];
+const [current, setCurrent] = useState(photoArray[0]['url'])
+
+const handleThumbnail = (url) => {
+  console.log('clicked: ', url);
+  setCurrent(url)
+};
+
 return (
-  <div className='col-span-1 row-span-1 flex justify-center'>
-    <img className='w-full h-full object-cover mx-4' src='https://images.unsplash.com/photo-1554260570-9140fd3b7614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'/>
+  <div className='flex justify-center'>
+    <img className='w-full h-full object-cover mx-4' src={current}/>
+    <div className='absolute top-1/2 left-36 transform -translate-y-1/2'>
+    {photoArray.map((photo,index) => {
+      return (
+      <img
+      key={photo['thumbnail_url']}
+      className={`border-2 w-16 h-16 mb-4`}
+      src={photo['thumbnail_url']}
+      onClick={() => handleThumbnail(photo['url'])}
+      />)
+    })}
+    </div>
   </div>
 )
 };
