8bff4f8feab3313597c23599fea0f343392e7087
ahusu <su.aust@gmail.com>
2023-06-28 14:35:39 -0700

    Outfit will no longer add a duplicate item

--- src/redux/reducers/outfitsReducer.js
+++ src/redux/reducers/outfitsReducer.js
@@ -2,9 +2,9 @@ const outfitsReducer = (state = [], action) => {
   //switch case of actions types where global state will be pulled from redux and change.
   switch(action.type){
     case 'ADD_OUTFIT_ITEM':
-      console.log('outfits before:',state)
+      console.log('outfits before:',state, 'adding...', action.payload)
+      if (state.indexOf(action.payload) != -1) return state;
       state = state.concat(action.payload);
-      console.log('outfits after:',state)
       return state;
     case 'REMOVE_OUTFIT_ITEM':
       let indexOfRemoved = state.indexOf(action.payload)
