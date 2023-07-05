da1ba48617ce3ab3cf6391f41233860b8928d200
pdaly91 <patrickdaly91@gmail.com>
2023-07-05 09:30:48 -0700

    index.html fix and styling change for the header

--- src/components/overview/Header.jsx
+++ src/components/overview/Header.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className='flex bg-gray-300 w-10/12 m-auto justify-center items-center border-2'>
-    <h1 className="text-3xl font-bold ml-28 my-3">The Mercury Store</h1>
+    <div className='flex bg-gray-700 text-gray-200 m-auto justify-between items-center'>
+      <h1 className="text-3xl font-bold ml-28 my-3 text-left italic">The Mercury Store</h1>
+      <div className="text-3xl font-thin flex flex-row mr-28 my-3">_________
+        <svg className="stroke-gray-300 w-6 h-6 ml-2 self-center" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7">
+            <title id="title">Search Icon</title>
+            <desc id="desc">A magnifying glass icon.</desc>
+            <g class="search-path" fill="none">
+              <path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/>
+              <circle cx="8" cy="8" r="7"/>
+            </g>
+        </svg>
+      </div>
     </div>
       )
 }
