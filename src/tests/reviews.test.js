f7c9d816d065e27490558cb2903b793711d2d7af
pdaly91 <patrickdaly91@gmail.com>
2023-07-05 12:02:58 -0700

    add tests for RatingSummary, ReviewBreakdown, and getMetrics

--- src/tests/reviews.test.js
+++ src/tests/reviews.test.js
@@ -1,24 +1,127 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import {Provider} from 'react-redux';
+import store from '../redux/store.js';
 import "@testing-library/jest-dom";
 
 import RatingsAndReviews from '../components/ratingsAndReviews/RatingsAndReviews.jsx';
 import RatingSummary from '../components/ratingsAndReviews/RatingSummary.jsx';
-import sampleReviewMeta from '../components/ratingsAndReviews/sampleReviewMeta.js';
+import getMetrics from '../components/ratingsAndReviews/getMetrics.js';
+import ReviewBreakdown from '../components/ratingsAndReviews/ReviewBreakdown.jsx';
+import ProductBreakdown from '../components/ratingsAndReviews/ProductBreakdown.jsx';
 
-describe("RatingSummary component meets business requirements", () => {
+describe(RatingsAndReviews, () => {
+  test("Ratings and Reviews renders to the page", () => {
+    render(
+      <Provider store={store}>
+        <RatingsAndReviews />
+      </Provider>
+    );
+    expect(screen.getByRole('heading')).toHaveTextContent('Ratings & Reviews');
+  })
+});
+
+describe(RatingSummary, () => {
+  const exampleMetaData = {
+    "product_id": "40346",
+    "ratings": {
+        "1": "25",
+        "2": "54",
+        "3": "53",
+        "4": "42",
+        "5": "94"
+    },
+    "recommended": {
+        "false": "70",
+        "true": "198"
+    },
+    "characteristics": {
+        "Fit": {
+            "id": 135224,
+            "value": "2.8423645320197044"
+        },
+        "Length": {
+            "id": 135225,
+            "value": "3.1219512195121951"
+        },
+        "Comfort": {
+            "id": 135226,
+            "value": "3.0000000000000000"
+        },
+        "Quality": {
+            "id": 135227,
+            "value": "3.3830845771144279"
+        }
+    }
+  };
+  test("Calculate Metrics Correctly", () => {
+    const exampleResult = getMetrics(exampleMetaData);
+    expect(exampleResult.calcCount).toEqual(268);
+    expect(exampleResult.calcAvg).toEqual('3.50');
+    expect(exampleResult.calcRecPct).toEqual('0.739');
+  })
+
   test("loads average rating", () => {
     render(
-      <RatingSummary metaData={sampleReviewMeta} />
+      <RatingSummary metaData={exampleMetaData} filters={[]} />
     )
     const summaryRating = screen.getByTestId("summary-average");
     expect(summaryRating).toBeInTheDocument();
   });
-});
 
-xdescribe("RatingSummary component meets business requirements", () => {
-  test("test example", () => {
-    //add test logic
-  });
+  test("Load Review Breakdown", () => {
+    const exampleResult = getMetrics(exampleMetaData);
+    render(<ReviewBreakdown ratings={exampleMetaData.ratings} count={exampleResult.calcCount} filters={[]} />)
+    const ratingBars = screen.getAllByText(/Stars/);
+
+    expect(screen.getByRole('table')).toBeInTheDocument();
+    expect(ratingBars.length).toEqual(5);
+  })
+
+  test("Load Filters to Review Breakdown", () => {
+    const exampleResult = getMetrics(exampleMetaData);
+    render(<ReviewBreakdown ratings={exampleMetaData.ratings} count={exampleResult.calcCount} filters={[3, 5]} />)
+
+    const filteredElement = screen.getByText(/Filtered/);
+    const resetButton = screen.getByText('Reset');
+    expect(filteredElement).toBeInTheDocument();
+    expect(resetButton).toBeInTheDocument();
+  })
+
+  test('Handle Clicks to all Star Bars in Review Breakdown', async () => {
+    const exampleResult = getMetrics(exampleMetaData);
+    let testClicked = [];
+    const testFn = (filter) => {
+      testClicked.push(filter);
+    }
+    render(<ReviewBreakdown ratings={exampleMetaData.ratings} count={exampleResult.calcCount} filters={[]} handleChangeFilters={testFn} />)
+
+    const fiveStar = screen.getByText('5 Stars');
+    const fourStar = screen.getByText('4 Stars');
+    const threeStar = screen.getByText('3 Stars');
+    const twoStar = screen.getByText('2 Stars');
+    const oneStar = screen.getByText('1 Stars');
+
+    await fireEvent.click(fiveStar);
+    await fireEvent.click(fourStar);
+    await fireEvent.click(threeStar);
+    await fireEvent.click(twoStar);
+    await fireEvent.click(oneStar);
+
+    expect(testClicked).toEqual([5, 4, 3, 2, 1]);
+  })
+
+  test('Handle Reset Click in Review Breakdown', async () => {
+    const exampleResult = getMetrics(exampleMetaData);
+    let testClicked = false;
+    const testFn = () => {
+      testClicked = true;
+    }
+    render(<ReviewBreakdown ratings={exampleMetaData.ratings} count={exampleResult.calcCount} filters={[5]} handleChangeFilters={testFn} />)
+
+    const button = screen.getByText('Reset');
+    await fireEvent.click(button);
+
+    expect(testClicked).toEqual(true);
+  })
 });
