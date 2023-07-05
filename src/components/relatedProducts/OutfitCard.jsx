3349360c728858d4c7ab6b5af4c38d057a173bf4
ahusu <su.aust@gmail.com>
2023-07-05 14:23:53 -0700

    outfit cards dont render when added, but when refresh, they persist

--- src/components/relatedProducts/OutfitCard.jsx
+++ src/components/relatedProducts/OutfitCard.jsx
@@ -9,17 +9,17 @@ const OutfitCard = ({ id }) => {
   const [stars, setStars] = useState(5);
   let starClicked = false;
   const dispatch = useDispatch();
-  let prodId = useSelector(state=>state.productId);
+  let prodId = useSelector(state => state.productId);
   let outfit = JSON.parse(localStorage.getItem('outfit'));
   const fetchCard = async () => {
     let card = await api.getCardInfo(id);
     setItemInfo(card);
   };
 
-  const addCard = ()=>{
+  const addCard = () => {
     if (!outfit.includes(prodId)) {
-    outfit = [...outfit, prodId]
-    localStorage.setItem('outfit', JSON.stringify(outfit));
+      outfit = [...outfit, prodId]
+      localStorage.setItem('outfit', JSON.stringify(outfit));
     }
   }
 
@@ -28,14 +28,14 @@ const OutfitCard = ({ id }) => {
   }, [])
 
   const star = () => {
-    starClicked =true
+    starClicked = true
     dispatch(setComparisonId(id));
     dispatch(comparisonModal());
     dispatch(openModal());
-    setTimeout(()=>starClicked =false, 10)
+    setTimeout(() => starClicked = false, 10)
   }
   const changeCard = () => {
-    setTimeout(()=>{
+    setTimeout(() => {
       if (starClicked === false) {
         dispatch(setId(id));
       }
@@ -44,32 +44,47 @@ const OutfitCard = ({ id }) => {
   if (id === undefined) {
     return (<>
       <div className='shrink-0 m-2 p-1 border-solid border-2 w-2/8 h-[250px]' data-testid="Outfit Card" onClick={() => { addCard() }}>
-          <div className='max-w-sm max-h-md'>
-            <div className='font-medium text-lg'>Click to add to Your Outfit</div>
-          </div>
+        <div classname='justify-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="plus">
+            <g data-name="Layer 2">
+              <g data-name="plus-square">
+                <rect width="24" height="24" opacity="0">
+                </rect>
+                <path d="M18 3H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm1 15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1z">
+                </path>
+                <path d="M15 11h-2V9a1 1 0 0 0-2 0v2H9a1 1 0 0 0 0 2h2v2a1 1 0 0 0 2 0v-2h2a1 1 0 0 0 0-2z">
+                </path>
+              </g>
+            </g>
+          </svg>
         </div>
+        <div className='max-w-sm max-h-md'>
+          <div className='font-medium text-xl'>Click to add to </div>
+          <div className='font-medium text-xl'>Your Outfit</div>
+        </div>
+      </div>
     </>)
   }
 
-return (
+  return (
     <>
       <div className='shrink-0 m-2 p-1 border-solid border-2 w-[200px]' data-testid="Outfit Card">
         <div onClick={() => { changeCard() }}>
           <div className='relative max-w-full bg-gray-300 '>
             <img className="w-full h-[150px] object-contain justify-center items-center" src={itemInfo.thumbnail} />
             <button onClick={() => { star() }}><svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 30 30"
-          className="absolute top-[1rem] right-[5%] w-10 h-10"
-        >
-          <path
-            fillRule="evenodd"
-            fill="gold"
-            d=" M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 30 30"
+              className="absolute top-[1rem] right-[5%] w-10 h-10"
+            >
+              <path
+                fillRule="evenodd"
+                fill="gold"
+                d=" M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
             l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z"
-            clipRule="evenodd"
-            stroke='black'
-          /></svg></button>
+                clipRule="evenodd"
+                stroke='black'
+              /></svg></button>
           </div>
           <div>
             <div className='italic text-m'>{itemInfo.category}</div>
@@ -77,13 +92,13 @@ return (
             <div>{itemInfo.slogan}</div>
             <div className='italic text-sm'>{itemInfo.price}</div>
             <div className="" >
-          {<StarRatings
-            rating={Number(stars)}
-            numberOfStars={5}
-            starDimension="15px"
-            starSpacing="2px"
-          />}
-        </div>
+              {<StarRatings
+                rating={Number(stars)}
+                numberOfStars={5}
+                starDimension="15px"
+                starSpacing="2px"
+              />}
+            </div>
           </div>
         </div>
       </div>
