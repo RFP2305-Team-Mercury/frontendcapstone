e72460cc0f2a7b158a0729023d998711da38ba01
ahusu <su.aust@gmail.com>
2023-07-05 10:33:59 -0700

    made shrink-0 for RP card.  Still trying to contain the img, after removing the shrink

--- src/components/relatedProducts/ComparisonModal.jsx
+++ src/components/relatedProducts/ComparisonModal.jsx
@@ -162,13 +162,6 @@ export default function ComparisonModal({ onClose, id }) {
               >
                 Close
               </button>
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => dispatch(closeModal())}
-              >
-                Save Changes
-              </button>
             </div>
           </div>
         </div>
