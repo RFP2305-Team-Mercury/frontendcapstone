d6bcfbd7272f92813ee3005ebf8b5521ccbcf775
Caroline Robbins <caroline.tawney@gmail.com>
2023-07-05 16:51:31 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,7 +32,6 @@ return (
     <QuestionsAndAnswers/>
     <RatingsAndReviews />
     <Modal isOpen={isOpen} type = {useSelector(state => state.modalType)} onClose = {() => dispatch(closeModal())}/>
-    <button onClick = {() => handleClick()}>Open Modal</button>{/* Eventually, move this button inside your component */}
   </div>
 )
 }
