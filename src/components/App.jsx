26f2c1cf88830ef3b14649a898f9c62f69de5e5c
Caroline Robbins <caroline.tawney@gmail.com>
2023-07-06 08:34:43 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,22 +8,8 @@ import {
   closeModal,
 } from "../redux/actions/index.js";
 import Modal from "./Modal.jsx";
-import React from "react";
-import { useState } from "react";
-import { Provider, useSelector, useDispatch } from "react-redux";
-import {
-  question,
-  answer,
-  openModal,
-  closeModal,
-} from "../redux/actions/index.js";
-import Modal from "./Modal.jsx";
 
 // import components
-import Overview from "./overview/Overview.jsx";
-import RPandOL from "./relatedProducts/RPandOL.jsx";
-import QuestionsAndAnswers from "./QuestionsAnswers/QuestionsAnswers.jsx";
-import RatingsAndReviews from "./ratingsAndReviews/RatingsAndReviews.jsx";
 import Overview from "./overview/Overview.jsx";
 import RPandOL from "./relatedProducts/RPandOL.jsx";
 import QuestionsAndAnswers from "./QuestionsAnswers/QuestionsAnswers.jsx";
