26ea19b71171149ed876219e508527dbdfd2e3ae
pdaly91 <patrickdaly91@gmail.com>
2023-07-06 14:33:28 -0700

    add more tests for new review modal

---
+++ src/tests/reviewsTestData.js
@@ -0,0 +1,88 @@
+export const sampleReviews = {
+  "product": "40344",
+  "page": 0,
+  "count": 4,
+  "results": [
+      {
+          "review_id": 1280250,
+          "rating": 3,
+          "summary": "I loved it",
+          "recommend": true,
+          "response": null,
+          "body": "this is a great onesie",
+          "date": "2023-07-03T00:00:00.000Z",
+          "reviewer_name": "onesielover",
+          "helpfulness": 0,
+          "photos": []
+      },
+      {
+          "review_id": 1280226,
+          "rating": 2,
+          "summary": "Eh idk it's not that great",
+          "recommend": false,
+          "response": null,
+          "body": "I wore this onesie to Coachella and everyone hated on it",
+          "date": "2023-06-29T00:00:00.000Z",
+          "reviewer_name": "coachellaCarl",
+          "helpfulness": 1,
+          "photos": []
+      },
+      {
+          "review_id": 1280225,
+          "rating": 2,
+          "summary": "Eh idk it's not that great",
+          "recommend": false,
+          "response": null,
+          "body": "I wore this onesie to Coachella and everyone hated on it",
+          "date": "2023-06-29T00:00:00.000Z",
+          "reviewer_name": "coachellaCarl",
+          "helpfulness": 5,
+          "photos": []
+      },
+      {
+          "review_id": 1280168,
+          "rating": 4,
+          "summary": "Couldn't find my baby",
+          "recommend": true,
+          "response": null,
+          "body": "it was a great fit for my baby, but the camo was too good and after a while I couldn't find my baby!",
+          "date": "2023-06-23T00:00:00.000Z",
+          "reviewer_name": "Molly",
+          "helpfulness": 27,
+          "photos": []
+      }
+  ]
+};
+
+export const exampleMetaData = {
+  "product_id": "40346",
+  "ratings": {
+      "1": "25",
+      "2": "54",
+      "3": "53",
+      "4": "42",
+      "5": "94"
+  },
+  "recommended": {
+      "false": "70",
+      "true": "198"
+  },
+  "characteristics": {
+      "Fit": {
+          "id": 135224,
+          "value": "2.8423645320197044"
+      },
+      "Length": {
+          "id": 135225,
+          "value": "3.1219512195121951"
+      },
+      "Comfort": {
+          "id": 135226,
+          "value": "3.0000000000000000"
+      },
+      "Quality": {
+          "id": 135227,
+          "value": "3.3830845771144279"
+      }
+  }
+};
