0286493b339eb6b84df937722f645781741c7717
Andy Moc <andymoc.com@gmail.com>
2023-07-05 19:39:05 -0700

    Saving Progress

--- src/components/relatedProducts/OutfitList.jsx
+++ src/components/relatedProducts/OutfitList.jsx
@@ -5,11 +5,13 @@ import { useSelector } from 'react-redux';
 import { setList } from '../../redux/actions'
 
 const OutfitList = () => {
-  const [outfit, setOutfit] = useState(JSON.parse(localStorage.getItem('outfit')));
-  if (outfit === null) {setOutfit([])}
+  const [outfit, setOutfit] = useState([]);
+
 
   useEffect(()=>{
     console.log('outfit in storage is ',outfit)
+    setOutfit(JSON.parse(localStorage.getItem('outfit')))
+    if (outfit === null) {setOutfit([])}
   },[])
 
   return (<><div className='text-lg text-gray-800 m-1 p-1'>Your Outfit</div><div className="flex justify-left overflow-x-scroll "><OutfitCard />{outfit.map((itemId) => (<OutfitCard key={itemId} id={itemId} />))}</div></>)
