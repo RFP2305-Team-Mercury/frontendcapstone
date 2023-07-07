42b6c6492ee64e33d5b3d34094a99507ae00ca55
carolinerobbins <caroline.tawney@gmail.com>
2023-07-06 17:27:30 -0700

    added dark mode class for tailwind

--- src/components/overview/Header.jsx
+++ src/components/overview/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+    document.documentElement.classList.toggle('dark');
+  };
   return (
     <div className='flex bg-gray-700 text-gray-200 m-auto justify-between items-center'>
       <h1 className="text-3xl font-bold ml-28 my-3 text-left italic">The Mercury Store</h1>
@@ -14,11 +20,9 @@ export default function Header() {
             </g>
         </svg>
       </div>
-        <label class="relative inline-flex items-center cursor-pointer">
-          <input type="checkbox" value="" class="sr-only peer" />
-          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
-        </label>
+      <button onClick={toggleDarkMode}>
+      {isDarkMode ? 'Disable Dark Mode' : 'Enable Dark Mode'}
+    </button>
     </div>
       )
 }
