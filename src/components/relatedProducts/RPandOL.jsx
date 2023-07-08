07b20de793b0c117dd6fd9a3cb754282b4f6ed42
pdaly91 <patrickdaly91@gmail.com>
2023-07-08 09:39:48 -0700

    change borders to modules

--- src/components/relatedProducts/RPandOL.jsx
+++ src/components/relatedProducts/RPandOL.jsx
@@ -4,7 +4,7 @@ import OutfitList from './OutfitList.jsx'
 
 const RPandOL = () => {
   return (
-    <div className='border-solid border-2 w-10/12 m-4 p-4 m-auto dark:bg-gray-600 dark:text-white'>
+    <div className='w-10/12 m-4 p-4 m-auto dark:bg-gray-600 dark:text-white border-b border-gray-200 dark:border-gray-400'>
       <div className='h-2/5 block'>
         {<RelatedProducts />}
       </div>
