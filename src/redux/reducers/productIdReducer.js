430c270a8306d9030b4f0f16bde1ad110997791c
ahusu <su.aust@gmail.com>
2023-07-08 11:32:34 -0700

    removed a console log

--- src/redux/reducers/productIdReducer.js
+++ src/redux/reducers/productIdReducer.js
@@ -3,7 +3,6 @@ const productIdReducer = (state = 40348, action) => {
   //switch case of actions types where global state will be pulled from redux and change.
   switch(action.type){
     case 'SET_ID':
-      console.log('Changing Product ID to :',action.payload)
       return state = action.payload;
     default:
       return state
